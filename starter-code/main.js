870fb4e5cad086db9fd181c8fe0634ad4d086ebe
Justin Samuels <Jblizm87@Justins-Air.attlocal.net>
2016-11-20 22:06:59 -0800

    Added conditions for cards in main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,16 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo && cardFour === false) {
+	alert("Sorry, try again.");
+}
+else if (cardOne && cardTwo === true) {
+	alert("You found a match!");
+}
+else if (cardOne && cardThree === false) {
+	alert("Sorry, try again.");
+}
+else {
+	alert("You found a match!");
+}
