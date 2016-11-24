28d72841994b1c761103c087392b150f1299b7a7
Justin Samuels <Jblizm87@Justins-Air.attlocal.net>
2016-11-23 20:57:38 -0800

    Created card elements using JavaScript and DOM.

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,7 +4,9 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo && cardFour === false) {
+
+
+/*if (cardTwo && cardFour === false) {
 	alert("Sorry, try again.");
 }
 else if (cardOne && cardTwo === true) {
@@ -15,4 +17,16 @@ else if (cardOne && cardThree === false) {
 }
 else {
 	alert("You found a match!");
+}*/
+
+var memBoard = document.getElementById('game-board');
+
+var createCards = function(){
+	var newCard = document.createElement('div');
+	newCard.setAttribute('class','card');
+	memBoard.appendChild(newCard);
+}
+
+for (var count=0; count< 4; count++){
+	createCards();
 }
