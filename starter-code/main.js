11bf4bcff9f2efec475e6006ae33e219569e7e37
Justin Samuels <Jblizm87@Justins-MacBook-Air.local>
2016-11-28 01:54:27 -0800

    Final draft of Unit Assignment 11.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,6 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
 /*if (cardTwo && cardFour === false) {
 	alert("Sorry, try again.");
@@ -21,12 +17,43 @@ else {
 
 var memBoard = document.getElementById('game-board');
 
-var createCards = function(){
-	var newCard = document.createElement('div');
-	newCard.setAttribute('class','card');
-	memBoard.appendChild(newCard);
-}
+var cardFlip = function(){
+		if (this.getAttribute('data-card') === 'king'){
+		this.innerHTML = '<img src="king_of_spades.png"/>';
+	}
+	else {
+		this.innerHTML = '<img src="queen_of_spades.jpg"/>';
+	}
 
+};
+
+var isItMatch = function(array){
+		if(array[0] === array[1]) {
+		alert("You found a match!");
+	}
+};
+
+var pickTwoCards = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length >= 2){
+		isItMatch(cardsInPlay);
+			cardsInPlay = [];
+	};
+};
+
+var createCards = function(cardNum){
+	var cardElement = document.createElement('div');
+	cardElement.setAttribute('class','card');
+	cardElement.setAttribute('data-card', cards[cardNum]);
+	memBoard.appendChild(cardElement);
+	cardElement.addEventListener('click', pickTwoCards);
+	cardElement.addEventListener('click', cardFlip);
+};
+
+var createBoard = function(){
 for (var count=0; count< 4; count++){
-	createCards();
-}
+	createCards(count);
+	};
+};
+
+createBoard();
